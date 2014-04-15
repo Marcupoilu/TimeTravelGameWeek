bbf8839cb88995b0b40a9a22978496acbe900b96
Caro <caroline.bedon@gmail.com>
2014-04-15 23:14:34 +0200

    téléporteurs qui se lient bien

--- scripts/Teleporteur.js
+++ scripts/Teleporteur.js
@@ -1,12 +1,22 @@
 define(function(require) {
-	var Teleporteur = function(x, y)
+	var Teleporteur = function(linkId, x, y)
 	{
+		this.linkId = linkId;
 		this.x = x;
 		this.y = y;
+		this.target = null;
 
 		this.setTarget = function(target){
 			this.target = target;
-		}
+		};
+
+		this.searchTarget = function(){
+			var layer = Game.map.layers[2];
+			_.each(this.tpTiles, function(tp){
+				var idLink = layer.data[tp.y][tp.x].index;
+				_this.teleporteurs.push(new Teleporteur(idLink, tp.x, tp.y));
+			});
+		};
 	}
 
 	return Teleporteur;
