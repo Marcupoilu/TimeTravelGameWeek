e1b4e41b0a1beb6e7277d726e30bbbc8479d5a2b
Caro <caroline.bedon@gmail.com>
2014-04-16 00:24:41 +0200

    début classe projection temporelle

---
+++ scripts/Projection.js
@@ -0,0 +1,38 @@
+define(function(require) {
+	
+	var Projection = function(trajet){
+		this.trajet = trajet || [];
+		this.currId = 0;
+
+		this.preload = function()
+		{
+			Game.load.spritesheet('projection', '../images/gabarit_chara.png', 64, 128, 1);
+		};
+
+		this.create = function(depart){
+			this.sprite = Game.add.sprite(depart.x * 64, depart.y * 64 - 64, 'projection');
+			Game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+		}
+
+		this.addCase = function(proCase)
+		{
+			this.trajet.push(proCase);
+		};
+
+		this.moveToNext = function()
+		{
+			console.log("currId = " + this.currId + ", this.trajet = ", this.trajet);
+			if(this.currId != this.trajet.length){
+				this.currId++;
+				this.moveToCase(this.trajet[this.currId]);
+			}
+		}
+
+		this.moveToCase = function(proCase)
+		{
+			console.log("je bouge à la case ", proCase);
+		}
+	}
+
+    return Projection;
+});
