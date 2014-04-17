1e48256baf2c0e91e9ada7d7954da8b21c0dabd4
UngererFabien <fabien@qunb.com>
2014-04-17 23:24:41 +0200

    main menu

--- scripts/mainMenu.js
+++ scripts/mainMenu.js
@@ -6,17 +6,20 @@ define(function(require) {
 			Game.load.image('mainMenu', '../images/MainMenu.png');	
 			Game.load.image('button' , '../images/button.png');
 
-		    this.style = { font: "30px Arial", fill: "#fff", align: "center" };
+		    this.style = { font: "30px Arial", fill: "#fff", 'text-align': 'center', width: '100px'};
 
 			this.destroyArray = [];
 		},
 
 		create: function(){
-			this.backgroundImg = Game.add.image('mainMenu');
-  			this.buttonPlay = Game.add.button(Game.world.centerX/2, 500, 'button', this.onClickPlay, this);
-    		this.buttonCredit = Game.add.button(Game.world.centerX/2, 700, 'button', this.onClickCredit, this);
-		  	this.tbuttonPlay = Game.add.text(Game.world.centerX/2, 490, 'Play', this.style);
-		  	this.tbuttonCredit = Game.add.text(Game.world.centerX/2, 490, 'Credit', this.style);
+			this.backgroundImg = Game.add.sprite(0, 0, 'mainMenu');
+  			this.buttonPlay = Game.add.button(Game.world.centerX/2 + 95, 270, 'button', this.onClickPlay, this);
+    		this.buttonCredit = Game.add.button(Game.world.centerX/2 + 95, 485, 'button', this.onClickCredit, this);
+		  	this.tbuttonPlay = Game.add.text(Game.world.centerX/2 + 250, 320, 'Play', this.style);
+		  	this.tbuttonCredit = Game.add.text(Game.world.centerX/2 + 240, 535, 'Credit', this.style);
+
+		  	this.buttonPlay.scale.setTo(0.85, 1.1);
+		  	this.buttonCredit.scale.setTo(0.85, 1.1);
 
 		 	Game.sprites.push(this.sprite, this.buttonNextLevel, this.buttonMainMenu, this.t, this.tMainMenu, this.tNextLevel)
 		},
@@ -30,7 +33,7 @@ define(function(require) {
 		},
 
 		onClickcredit: function(){
-			console.log('welcome in the Tardis');
+			console.log('Welcome in the Tardis');
 		},
 
 		onDestroy: function(){
